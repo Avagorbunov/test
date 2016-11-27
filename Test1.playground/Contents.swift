ba54091dd57a47ca947e11ccf631495a9aff45b6
Avagorbunov <avagorbunov@gmail.com>
2016-11-27 18:56:54 +0300

    test1

---
+++ Test1.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+
+
+
+var a = 3
+
+if (a < 10) {
+    print ("верно")
+
+    
+} else {
+    print("не верно")
+}
